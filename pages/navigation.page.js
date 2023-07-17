a41bdb7536a9d92f7a82b051411033321e6e2eaf
buvaneswaranPersonalAccount <139749227+buvaneswaranPersonalAccount@users.noreply.github.com>
2023-07-17 15:45:56 +0530

    first commit

---
+++ pages/navigation.page.js
@@ -0,0 +1,61 @@
+export class navigation {
+  // locator
+  constructor(page) {
+    this.page = page;
+    this.integrationBtn = page.locator("#sidebar-app-integration");
+    this.integratedApps = '[class="container"] li h6';
+    this.integratedApp = page.locator('[class="container"] li');
+    this.disConnectBtn = page.locator(
+      '[class="awd-btn awd-btn-tertiary awd-btn--sm fx fx-justify-center mt-auto "]'
+    );
+    this.connectBtn = page.locator(
+      '[class="awd-btn awd-btn-primary awd-btn--sm mt-2 fx fx-justify-center activateBtn "]'
+    );
+    this.aboutPara = page.locator('[class="awd-text--sm awd-tc-grey--700"]');
+  }
+
+  //actions
+
+  async navigateToIntegration() {
+    await this.integrationBtn.click();
+    await this.page.waitForTimeout(5000);
+  }
+
+  async navigateToApp(givenApp) {
+    const apps = await this.page.$$(this.integratedApps);
+    let i = 0;
+    for (let app of apps) {
+      const appText = await app.textContent();
+
+      if (appText.startsWith(givenApp)) {
+        return this.integratedApp.nth(i).click();
+      }
+      i++;
+    }
+  }
+
+  async verifyTheApp(givenApp) {
+    const aboutPara = await this.aboutPara.textContent();
+
+    if ((await this.connectBtn.isHidden())) {
+      const connectText = await this.connectBtn.textContent();
+
+      if (connectText.startsWith("Connect")) {
+        console.log(givenApp + " : this application is disconnected");
+        console.log("about " + givenApp + "  :" + aboutPara);
+      }
+    } else if (!(await this.disConnectBtn.isHidden())) {
+      const disconnectText = await this.disConnectBtn.textContent();
+
+      if (disconnectText.startsWith("Disconnect")) {
+        console.log(givenApp + " : this application is connected");
+        console.log("about " + givenApp + "  :" + aboutPara);
+      }
+    } else {
+      console.log(
+        givenApp + " : this application doesn't have connection functionality"
+      );
+      console.log("about " + givenApp + "  :" + aboutPara);
+    }
+  }
+}
---
+++ tests/loginAppointment.test.js
@@ -0,0 +1,59 @@
+import { test, expect } from "@playwright/test";
+import { loginPage } from "../pages/login.page.js";
+import { dateSelectPage } from "../pages/dateSelect.page.js";
+import { bookingAppointment } from "../pages/booking.page.js";
+const constant = require("../constant.js");
+
+let page;
+let login;
+let dateSelect;
+let booking;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  login = new loginPage(page);
+  dateSelect = new dateSelectPage(page);
+  booking = new bookingAppointment(page);
+
+  await login.goTo(constant.URL);
+});
+
+test.afterAll(async () => {
+  await page.waitForTimeout(10000);
+  await page.close();
+});
+
+test.describe("login and booking appoinment", () => {
+  test("verify login using valid credentials", async () => {
+    await login.doLogin(constant.userName, constant.passWord);
+    await expect(await login.verifyProfileName()).toEqual(constant.profileName);
+  });
+
+  test("select of year, month, date", async () => {
+    await dateSelect.viewTypeSelect();
+    await dateSelect.selectMonthYear(constant.month, constant.year);
+    await dateSelect.selectDate(constant.year, constant.month, constant.date);
+  });
+
+  test("book appoinment with given credentials", async () => {
+    //await booking.bookAppointmentService(constant.cost, constant.hours , constant.serviceName);
+    await booking.bookService(constant.serviceName);
+    await booking.bookTime(constant.cost, constant.hours);
+    await booking.bookAppointmentService1(constant.time);
+    await booking.recurringLists(constant.recurringShift);
+    await booking.customSelect(
+      constant.counts,
+      constant.repeatShift,
+      constant.recurringShift
+    );
+    await booking.endRepeat(
+      constant.endRepeatText,
+      constant.endRepeatNum,
+      constant.recurringShift
+    );
+    await booking.addguest(constant.guestId);
+    await booking.teleport(constant.needed);
+    await booking.notes(constant.message);
+    await booking.create();
+  });
+});
